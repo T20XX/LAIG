5cac034c8405644b5dd4f9f686b4956639fd7af9
Telmo Barros <telmo.20@hotmail.com>
2016-11-15 00:04:40 +0000

    animations working fine

--- TP2/parser/graphData/CircularAnimation.js
+++ TP2/parser/graphData/CircularAnimation.js
@@ -8,8 +8,8 @@ function CircularAnimation(span, centerX, centerY, centerZ, radius, startAng, ro
     this.centerY = centerY;
     this.centerZ = centerZ;
     this.radius = radius;
-    this.startAng = startAng;
-    this.rotAng = rotAng;
+    this.startAng = startAng*Math.PI/180;
+    this.rotAng = rotAng*Math.PI/180;
 }
 
 CircularAnimation.prototype = Object.create(Animation.prototype);
@@ -46,6 +46,16 @@ CircularAnimation.prototype.getRotAng=function()
 };
 
 CircularAnimation.prototype.getTransformation = function(deltaTime){
-	return 0;
+    var transf = mat4.create();
+	
+	mat4.translate(transf, transf, vec3.fromValues(this.centerX, this.centerY, this.centerZ));
+	if (deltaTime > this.span)
+		mat4.rotate(transf, transf, this.startAng + this.rotAng, [0, 1, 0]);
+	else
+		mat4.rotate(transf, transf, this.startAng + (deltaTime / this.span) * this.rotAng, [0, 1, 0]);
+	mat4.translate(transf, transf, [this.radius, 0, 0]);
+	//if(this.rotAng > 0)
+		//mat4.rotate(transf, transf, Math.PI, [0, 1, 0]);
+	return transf;
 	
 }
--- TP2/parser/graphData/LinearAnimation.js
+++ TP2/parser/graphData/LinearAnimation.js
@@ -14,19 +14,22 @@ function LinearAnimation(span, controlPoints) {
     }
     this.velocity = this.totalDistance / this.span;
 }
+
 LinearAnimation.prototype = Object.create(Animation.prototype);
 LinearAnimation.prototype.constructor = LinearAnimation;
 LinearAnimation.prototype.getControlPoints = function() {
     return this.controlPoints;
 }
+
 LinearAnimation.prototype.setControlPoints = function(controlPoints) {
     this.controlPoints = controlPoints;
 }
+
 LinearAnimation.prototype.getTransformation = function(deltaTime) {
     var transf = mat4.create();
     if (deltaTime >= this.span) {
         mat4.translate(transf, transf, this.controlPoints[this.controlPoints.length - 1]);
-        //mat4.rotate(transf, transf, this.calculateAngle(this.controlPoints[this.controlPoints.length - 2], this.controlPoints[this.controlPoints.length - 1]), [0, 1, 0]);
+        mat4.rotate(transf, transf, this.calculateAngle(this.controlPoints[this.controlPoints.length - 2], this.controlPoints[this.controlPoints.length - 1]), [0, 1, 0]);
     }else if (deltaTime >= 0){
     var interPoint;
     var angle;
@@ -39,7 +42,8 @@ LinearAnimation.prototype.getTransformation = function(deltaTime) {
         if (deltaTime >= ((tempDistance + this.partialsDistances[i]) / this.velocity)) {
             tempDistance += this.partialsDistances[i];
         } else {
-        	var t = (this.partialsDistances[i] - (tempDistance - (deltaTime * this.velocity))) / this.partialsDistances[i]; 
+        	//var t = (this.partialsDistances[i] - (tempDistance - (deltaTime * this.velocity))) / this.partialsDistances[i]; 
+           	var t = ((deltaTime * this.velocity) - tempDistance) / this.partialsDistances[i];
             interPoint = this.calculateInterpolatePoint(this.controlPoints[i], this.controlPoints[i + 1], t);
             angle = this.calculateAngle(this.controlPoints[i], this.controlPoints[i + 1]);
             break;
@@ -52,6 +56,7 @@ LinearAnimation.prototype.getTransformation = function(deltaTime) {
     console.log(transf);
     return transf;
 }
+
 LinearAnimation.prototype.parseControlPoints = function(cP) {
     var result = [];
     for (var i = 0; i < cP.length; i++) {
@@ -60,14 +65,16 @@ LinearAnimation.prototype.parseControlPoints = function(cP) {
     console.log(result);
     return result;
 }
+
 LinearAnimation.prototype.calculateDistance = function(p1, p2) {
     return Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2) + Math.pow(p2[2] - p1[2], 2));
 }
+
 LinearAnimation.prototype.calculateAngle = function(p1, p2) {
     return Math.atan2(p2[0] - p1[0], p2[2] - p1[2]);
 }
-LinearAnimation.prototype.calculateInterpolatePoint = function(p1, p2, t) {
 
+LinearAnimation.prototype.calculateInterpolatePoint = function(p1, p2, t) {
 var result = [];
 	for (var i = 0; i < p1.length; i++)
 	{
